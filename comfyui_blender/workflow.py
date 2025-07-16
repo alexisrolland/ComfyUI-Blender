0df57211c1a07a14270e75c1f95ca606c7c86d89
Alexis Rolland <alexis.rolland@ubisoft.com>
2025-07-16 18:28:08 +0800

    Manage bad default value in combo input

--- comfyui_blender/workflow.py
+++ comfyui_blender/workflow.py
@@ -34,10 +34,15 @@ def create_class_properties(dictionary):
 
         # Combo box
         if node["class_type"] == "BlenderInputCombo":
+            # If default value not in list, set to first item in the list
+            default = node["inputs"].get("default", "")
+            items = node["inputs"]["list"].split("\n")
+            if default not in items:
+                default = items[0]
             properties[property_name] = EnumProperty(
                 name=name,
-                default=node["inputs"].get("default", ""),
-                items=[(i, i, "") for i in node["inputs"]["list"].split("\n")]
+                default=default,
+                items=[(i, i, "") for i in items]
             )
             continue
 
@@ -109,9 +114,7 @@ def create_workflow_class(workflow_filename, properties):
     """Create a new PropertyGroup class for a workflow."""
 
     # Generate a class name from the workflow file name
-    workflow_name = os.path.splitext(workflow_filename)[0]
-    class_name = f"wkf_{workflow_name}"
-    class_name = re.sub(r"[^a-zA-Z0-9_]", "_", class_name).lower()
+    class_name = get_workflow_class_name(workflow_filename)
 
     # Create the new class
     new_class = type(class_name, (bpy.types.PropertyGroup,), {})
@@ -123,6 +126,14 @@ def create_workflow_class(workflow_filename, properties):
     # Manually add the annotations attribute
     new_class.__annotations__ = {prop_name: properties[prop_name] for prop_name in properties}
     return new_class
+
+def get_workflow_class_name(workflow_filename):
+    """Generate a class name from the workflow file name."""
+
+    workflow_name = os.path.splitext(workflow_filename)[0]
+    class_name = f"wkf_{workflow_name}"
+    class_name = re.sub(r"[^a-zA-Z0-9_]", "_", class_name).lower()
+    return class_name
 
 def get_workflow_list(self, context):
     """Return a list of workflow JSON files from the workflows folder."""
@@ -201,5 +212,5 @@ def register(workflow_class):
 def unregister(workflow_class):
     """Unregister the panel."""
 
-    bpy.utils.unregister_class(workflow_class)
     del bpy.types.Scene.current_workflow
+    bpy.utils.unregister_class(workflow_class)
