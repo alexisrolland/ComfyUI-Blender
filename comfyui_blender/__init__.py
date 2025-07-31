694334f034470d22980867dffeeb224882709098
Alexis Rolland <alexis.rolland@ubisoft.com>
2025-08-01 05:00:38 +0800

    Fix permission issue when rendering images

--- comfyui_blender/__init__.py
+++ comfyui_blender/__init__.py
@@ -45,7 +45,7 @@ from . import settings
 bl_info = {
     "name": "ComfyUI Blender",
     "author": "Alexis ROLLAND",
-    "version": (0, 5, 0),
+    "version": (0, 5, 1),
     "blender": (4, 4, 3),
     "location": "View3D > Sidebar > ComfyUI",
     "description": "Blender add-on to send requests to a ComfyUI server.",
