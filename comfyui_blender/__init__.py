905e212725ffa2a5a733d8823029e605ee5d9e34
Alexis ROLLAND <alexisrolland@hotmail.com>
2025-08-06 23:46:01 +0800

    Update version to 0.6.0

--- comfyui_blender/__init__.py
+++ comfyui_blender/__init__.py
@@ -47,7 +47,7 @@ from . import settings
 bl_info = {
     "name": "ComfyUI Blender",
     "author": "Alexis ROLLAND",
-    "version": (0, 5, 1),
+    "version": (0, 6, 0),
     "blender": (4, 4, 3),
     "location": "View3D > Sidebar > ComfyUI",
     "description": "Blender add-on to send requests to a ComfyUI server.",
