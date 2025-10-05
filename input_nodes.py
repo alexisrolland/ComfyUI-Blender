974e58b386addde0d8461723620f998961e7d4aa
Alexis ROLLAND <alexisrolland@hotmail.com>
2025-10-05 22:20:08 +0800

    Add Blender Input Sampler node

--- input_nodes.py
+++ input_nodes.py
@@ -2,6 +2,7 @@ import os
 
 import folder_paths
 from comfy_api.latest import io
+from comfy_extras.nodes_custom_sampler import KSamplerSelect
 from comfy_extras.nodes_primitive import Boolean, Float, Int, String, StringMultiline
 from comfy.comfy_types.node_typing import IO
 from nodes import CheckpointLoaderSimple, LoadImage, LoraLoaderModelOnly, UNETLoader
@@ -148,6 +149,28 @@ class BlenderInputInt(Int):
     @classmethod
     def execute(cls, value: int, order: int, default: int, min: int, max: int, step: int, camera_width: bool, camera_height: bool, **kwargs) -> io.NodeOutput:
         return io.NodeOutput(value)
+
+
+class BlenderInputSampler(KSamplerSelect):
+    """Display the list of samplers in the Blender add-on."""
+    CATEGORY = "blender"
+    FUNCTION = "execute"
+    RETURN_TYPES = (IO.ANY,)
+
+    @classmethod
+    def INPUT_TYPES(s):
+        INPUT_TYPES = super().INPUT_TYPES()
+        INPUT_TYPES["required"]["order"] = (IO.INT, {"default": 0, "min": MIN_INT, "max": MAX_INT, "control_after_generate": False})
+        INPUT_TYPES["required"]["default"] = (IO.STRING, {"default": ""})
+
+        # Create optional input key
+        if not INPUT_TYPES.get("optional", None):
+            INPUT_TYPES["optional"] = {}
+        INPUT_TYPES["optional"]["group"] = ("GROUP", {"forceInput":True})
+        return INPUT_TYPES
+
+    def execute(self, sampler_name, order: int, default: str, **kwargs):
+        return super().get_sampler(sampler_name)
 
 
 class BlenderInputLoad3D():
