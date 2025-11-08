b0c9197d315cbbcceb738303c5a4a17344c015f7
Alexis ROLLAND <alexisrolland@hotmail.com>
2025-11-08 20:45:42 +0800

    Add custom node BlenderOutputString

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -16,6 +16,7 @@ from .blender_input_string_multiline import *
 from .blender_output_download_3d import *
 from .blender_output_save_glb import *
 from .blender_output_save_image import *
+from .blender_output_string import *
 
 __all__ = [
     "BlenderInputBoolean",
@@ -35,5 +36,6 @@ __all__ = [
     "BlenderInputStringMultiline",
     "BlenderOutputDownload3D",
     "BlenderOutputSaveGlb",
-    "BlenderOutputSaveImage"
+    "BlenderOutputSaveImage",
+    "BlenderOutputString"
 ]
