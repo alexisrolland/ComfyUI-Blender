557a5e4fe6bb6ac44cee5ae607daf346923b07e3
Alexis ROLLAND <alexisrolland@hotmail.com>
2025-12-31 21:35:12 +0800

    Add label when no compositor is found

--- comfyui_blender/menus/custom_compositor_menu.py
+++ comfyui_blender/menus/custom_compositor_menu.py
@@ -11,16 +11,20 @@ class ComfyBlenderCustomCompositorMenu(bpy.types.Menu):
     def draw(self, context):
         layout = self.layout
 
-        # Custom compositors
-        layout.label(text="Custom Compositors", icon="NODE_COMPOSITING")
+        # Check if there are any compositor node trees
+        compositors = [compositor for compositor in bpy.data.node_groups if compositor.type == "COMPOSITING"]
 
-        # Loop over all node groups in the blend file
-        for node_tree in bpy.data.node_groups:
-            # Check if it's a compositor node tree
-            if node_tree.type == "COMPOSITING":
+        if not compositors:
+            layout.label(text="No custom compositors found", icon="NODE_COMPOSITING")
+        else:
+            # Custom compositors
+            layout.label(text="Custom Compositors", icon="NODE_COMPOSITING")
+
+            # Loop over all node groups in the blend file
+            for compositor in compositors:
                 row = layout.row()
-                custom_render = row.operator("comfy.render_custom_compositor", text=node_tree.name)
-                custom_render.compositor_name = node_tree.name
+                custom_render = row.operator("comfy.render_custom_compositor", text=compositor.name)
+                custom_render.compositor_name = compositor.name
                 custom_render.workflow_property = context.scene.comfyui_menu_workflow_property
 
 
